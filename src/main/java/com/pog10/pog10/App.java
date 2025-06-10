f7f3b770d17e138cfb0187ed1a7561bb18c0d9f4
Praveenkumarjs123 <praveenkumarjs250@gmail.com>
2025-06-10 19:32:57 +0530

    azure pipeline

---
+++ src/main/java/com/pog10/pog10/App.java
@@ -0,0 +1,13 @@
+package com.pog10.pog10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
